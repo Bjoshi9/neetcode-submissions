855d4fe0084c04877b9c351ab3443960a7192935
Brijesh Joshi <joshi.brijesh.ce@gmail.com>
2026-06-01 14:46:29 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val result = IntArray(nums.size) { 1 }
+        var prefix = nums[0]
+        
+        for(i in 1 until nums.size) {
+            result[i] = prefix
+            prefix *= nums[i]
+        }
+        
+        var suffix = 1
+        for(i in (nums.size-1) downTo 0) {
+            result[i] *= suffix
+            suffix *= nums[i]
+        }
+            
+        return result
+    }
+}
